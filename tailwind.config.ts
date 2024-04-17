4ebae0b7f78888b1af6c9110af323bda0019ada6
thanhnd2512@gmail.com <thanhnd2512@gmail.com>
2024-04-17 16:39:50 +0700

    config project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        p: '240px',
+        d: '1024px',
+        lg: '1650px',
       },
+      fontFamily: {
+        LexendDeca: ['Lexend Deca', 'sans-serif'],
+      },
+
+      colors: {},
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('tailwind-scrollbar')],
+};
+export default config;
